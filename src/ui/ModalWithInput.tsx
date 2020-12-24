cc72b1029829eb4d94b385684aff04756d058fcf
Marios Galanis <mariosgalanis@outlook.com>
2020-12-24 20:20:09 +0200

    add modalWithInput component

---
+++ src/ui/ModalWithInput.tsx
@@ -0,0 +1,60 @@
+import React, { CSSProperties } from 'react';
+import Modal from 'react-modal';
+import { Button } from './Button';
+import { modalStyles } from './modalStyles';
+import { View } from './View';
+
+type Props = {
+  show: boolean;
+  name: string;
+  placeholder: string;
+  onClose: () => void;
+  onCreate: () => void;
+  onChange: (e: any) => void;
+};
+
+export function ModalWithInput(props: Props) {
+  const { show, name, placeholder, onClose, onCreate, onChange } = props;
+
+  return (
+    <Modal isOpen={show} ariaHideApp={false} onRequestClose={onClose} style={modalStyles}>
+      <input
+        style={styles.input}
+        type="text"
+        value={name}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      <View>
+        <Button
+          onClick={onClose}
+          title="Cancel"
+          variant="primary"
+          style={styles.button}
+        />
+        <Button
+          onClick={onCreate}
+          title="Create"
+          variant="secondary"
+          style={styles.button}
+        />
+      </View>
+    </Modal>
+  );
+}
+
+type Styles = {
+  input: CSSProperties;
+  button: CSSProperties;
+};
+
+const styles: Styles = {
+  input: {
+    padding: '10px',
+    fontSize: '1rem'
+  },
+
+  button: {
+    margin: '20px 10px 0 10px'
+  }
+};
---
+++ src/ui/modalStyles.ts
@@ -0,0 +1,14 @@
+import { theme } from './theme';
+
+export const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: theme.colors.light,
+    opacity: 1
+  }
+};
