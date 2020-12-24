4d66bb9a8881393c563e3eca915de9285e26bc76
Marios Galanis <mariosgalanis@outlook.com>
2020-12-24 19:09:05 +0200

    add theme

---
+++ src/ui/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    light: '#fff4f2',
+    dark: '#c4c4c4',
+    black: '#000',
+    white: '#fff',
+    primary: '#ffdfdf',
+    secondary: '#a7fcdc',
+    alternative: '#fffedf',
+    labels: {
+      green: '#317140',
+      red: '#f51720',
+      orange: '#ffb067',
+      blue: '#2eb5e0'
+    }
+  },
+  shadow: {
+    primary: '0 0 10px rgba(0, 0, 0, 0.1)',
+    big: '0 0 10px rgba(0, 0, 0, 0.5)'
+  },
+  fonts: {
+    primary: "'Source Sans Pro', sans-serif"
+  }
+};
