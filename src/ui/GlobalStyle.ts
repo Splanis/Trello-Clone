18ff6ffca7786174d4f71c4f6ac66154a025d3a4
Marios Galanis <mariosgalanis@outlook.com>
2020-12-24 19:32:08 +0200

    add Global Style

---
+++ src/ui/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+import { theme } from './theme';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box
+    }
+
+    body {
+        background-color: ${theme.colors.light};
+        font-family:  ${theme.fonts.primary};
+        height: 100vh;
+        
+    }
+`;
