a01ec8dd3e3045576608dbd7c806a562249f7548
Marios Galanis <mariosgalanis@outlook.com>
2020-12-24 20:29:57 +0200

    add Card Component

---
+++ src/pages/board/Card.tsx
@@ -0,0 +1,75 @@
+import React, { CSSProperties } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import { ICard } from '../../redux/models/Board';
+import { Text } from '../../ui/Text';
+import { theme } from '../../ui/theme';
+import { View } from '../../ui/View';
+
+type Props = {
+  card: ICard;
+  index: number;
+  onClick?: () => void;
+};
+
+export function Card(props: Props) {
+  const { card, index, onClick } = props;
+
+  return (
+    <Draggable draggableId={card.id} index={index}>
+      {(provided, snapshot) => (
+        <CardStyled
+          onClick={onClick}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          isDragging={snapshot.isDragging}>
+          <Text text={card.name} style={styles.text} />
+          <View justify="flex-start" style={{ marginTop: 10 }}>
+            {card.labels &&
+              card.labels.map((l) => (
+                <Text
+                  style={{ ...styles.label, backgroundColor: l.color }}
+                  text={l.label}
+                />
+              ))}
+          </View>
+        </CardStyled>
+      )}
+    </Draggable>
+  );
+}
+
+// QUICK-FIX: Can't add inline styles
+const CardStyled = styled.div<{ isDragging: boolean }>`
+  padding: 10px;
+  background: ${({ isDragging }) =>
+    isDragging ? theme.colors.secondary : theme.colors.alternative};
+  border-radius: 6px;
+  margin: 10px;
+`;
+
+type Styles = {
+  card: CSSProperties;
+  label: CSSProperties;
+  text: CSSProperties;
+};
+
+const styles: Styles = {
+  card: {
+    padding: '10px',
+    borderRadius: '6px',
+    margin: '10px'
+  },
+
+  text: {
+    wordBreak: 'break-all'
+  },
+
+  label: {
+    margin: '0 5px',
+    padding: '2px 12px',
+    borderRadius: 30,
+    color: 'white'
+  }
+};
